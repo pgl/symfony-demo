08d8a0979c261aa9101b7ba0dae061e305751069
Peter Lowe <pgl@yoyo.org>
2019-04-03 15:54:38 +0200

    Updated DomainController as per php-cs

--- src/Controller/DomainController.php
+++ src/Controller/DomainController.php
@@ -1,16 +1,24 @@
 <?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Controller used to test domains.
  *
  * @Route("/domain")
- *
  */
 class DomainController extends Controller
 {
@@ -29,5 +37,4 @@ class DomainController extends Controller
     {
         return Response::create('SUBDOMAIN '.$subdomain);
     }
-
 }
